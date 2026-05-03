89155b0260bab2ad191eb84336ff3db53523b70d
washington panganai <washpangs@gmail.com>
2026-05-03 16:34:49 +0100

    test

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ.get("MAIL_USERNAME")
 
 #test
+#test
 app.config['MAIL_PASSWORD'] = os.environ.get("MAIL_PASSWORD")
 
 mail = Mail(app)
