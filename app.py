1959df3628d30a86f412672597b1d6ab53db5945
washington panganai <washpangs@gmail.com>
2026-05-03 16:32:40 +0100

    test

--- app.py
+++ app.py
@@ -16,6 +16,8 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ.get("MAIL_USERNAME")
+
+#test
 app.config['MAIL_PASSWORD'] = os.environ.get("MAIL_PASSWORD")
 
 mail = Mail(app)
